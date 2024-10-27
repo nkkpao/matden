1f41965889b8a2f243f30e350dcb808dcb300f56
nkkpao <99089421+nkkpao@users.noreply.github.com>
2024-10-27 15:36:42 +0700

    Update AxaxaComponent.jsx

--- src/components/AxaxaComponent.jsx
+++ src/components/AxaxaComponent.jsx
@@ -1,30 +1,57 @@
 import React, { useState, useEffect, useRef } from "react";
 
 export const AxaxaComponent = () => {
-  const [axa, setAxa] = useState("axaxa");
+  const initialAxaxaState = "AXAXA";
+  const initialIntervalState = 1000;
+  let addedString = "xa";
+
+  const [axa, setAxa] = useState(initialAxaxaState);
+  const [intervalTime, setIntervalTime] = useState(initialIntervalState);
 
   const timer = useRef(null);
 
   useEffect(() => {
-    startTimer();
+    handlePlay();
   });
 
-  function startTimer() {
+  function plusHa() {
+    setAxa((axa) => (axa += addedString));
+  }
+
+  function handlePlay() {
     if (timer.current) {
       clearInterval(timer.current);
     }
     const callback = plusHa;
-    timer.current = setInterval(callback, 1000);
+    timer.current = setInterval(callback, intervalTime);
   }
 
-  function plusHa() {
-    setAxa((axa) => (axa += "xa"));
+  function handlePause() {
+    clearInterval(timer.current);
+  }
+
+  function handleReboot() {
+    setAxa((axa) => (axa = initialAxaxaState));
+  }
+
+  function handleSlower() {
+    setIntervalTime((interval) => (interval *= 2));
+    handlePlay();
+  }
+
+  function handleFaster() {
+    setIntervalTime((interval) => (interval /= 2));
+    handlePlay();
   }
 
   return (
     <div className="axaxa">
       <span>{axa}</span>
-      <button onClick={plusHa}>+xa</button>
+      <button onClick={handleReboot}>reboot</button>
+      <button onClick={handlePlay}>play</button>
+      <button onClick={handlePause}>pause</button>
+      <button onClick={handleFaster}>faster</button>
+      <button onClick={handleSlower}>slower</button>
     </div>
   );
 };
